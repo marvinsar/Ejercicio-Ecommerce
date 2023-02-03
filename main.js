9c37d5b987770650c3273f6fd243ad6ca60d6739
Marvinsar <marvinsarsito@gmail.com>
2023-02-03 17:19:00 -0500

    se agrego la funcion del carrito compra, conservando responsil para mobile y desktop

--- main.js
+++ main.js
@@ -2,16 +2,47 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
 const menuHanIcon = document.querySelector('.menu');
+
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+
 const mobileMenu = document.querySelector('.mobile-menu');
+
+const aside = document.querySelector('.product-datail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHanIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed){
+        aside.classList.add('inactive')
+    }
+   
+
+
     desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed){
+        aside.classList.add('inactive')
+    }
     mobileMenu.classList.toggle('inactive')
     
 }
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+   
+
+if (!isMobileMenuClosed){
+    mobileMenu.classList.add('inactive')
+} 
+
+    aside.classList.toggle('inactive');
+}
+   
